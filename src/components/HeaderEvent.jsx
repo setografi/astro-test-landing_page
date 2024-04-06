329d5f7d50fa322e767e035bc0758263d4a28f56
setografi <hitonaashbell@gmail.com>
2024-04-06 20:55:57 +0700

    Update About Me Responsif 3

--- src/components/HeaderEvent.jsx
+++ src/components/HeaderEvent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HeaderEvent() {
   return (
     <>
-      <div className="fixed top-0 left-0 hidden lg:flex items-center justify-center p-2 lg:p-4 bg-BlackText h-screen border-r-2 border-black z-20">
+      <div className="fixed top-0 left-0 hidden lg:flex items-center justify-center p-2 lg:p-4 bg-BlackText h-[100dvh] border-r-2 border-black z-20">
         <button className="text-WhiteBack text-3xl font-medium cursor-pointer duration-300 transition-all hover:scale-125">
           <i className="ri-more-2-line"></i>
         </button>
