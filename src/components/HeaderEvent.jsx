0ea44d24a7d35dcd742d1d728a4dc3bf167c9d9b
setografi <hitonaashbell@gmail.com>
2024-04-13 22:44:52 +0700

    update: Animation Effects

--- src/components/HeaderEvent.jsx
+++ src/components/HeaderEvent.jsx
@@ -4,7 +4,13 @@ function HeaderEvent() {
   return (
     <>
       <div className="fixed top-0 left-0 hidden lg:flex items-center justify-center p-2 lg:p-4 bg-BlackText h-[100dvh] border-r-2 border-black z-20">
-        <button className="text-WhiteBack text-3xl font-medium cursor-pointer duration-300 transition-all hover:scale-125">
+        <button className="text-WhiteBack text-3xl font-bold cursor-pointer duration-300 transition-all hover:scale-125">
+          <i className="ri-more-2-line"></i>
+        </button>
+      </div>
+
+      <div className="fixed top-4 left-4 lg:hidden items-center justify-center px-4 py-3 bg-BlackText rounded-full z-20">
+        <button className="text-WhiteBack text-xl font-bold cursor-pointer duration-300 transition-all hover:scale-125">
           <i className="ri-more-2-line"></i>
         </button>
       </div>
